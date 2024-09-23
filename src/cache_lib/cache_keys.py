cd6542d2e38f0765c6a84470c9231b0834844c92
Nazar Topolsky <nazar.topolsky@vilmate.com>
2024-09-23 12:43:47 +0300

    Fixed crashing cache key generation for requests without parameters

--- src/cache_lib/cache_keys.py
+++ src/cache_lib/cache_keys.py
@@ -1,10 +1,16 @@
+from typing import Optional
+
+
 def get_default_request_cache_key(
     url: str,
     method: str,
-    params: dict,
+    params: Optional[dict],
 ):
     """
     Default key generation for requests. url is assumed to be without parameters
     """
-    sorted_params = '|'.join(sorted(f'{k}_{v}' for k, v in params.items()))
+    if params:
+        sorted_params = '|'.join(sorted(f'{k}_{v}' for k, v in params.items()))
+    else:
+        sorted_params = '|'
     return f'{url}!{method}!{sorted_params}'
