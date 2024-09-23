e3dc8c219eccca02afb65cf6e6ca868d01398a4b
Nazar Topolsky <nazar.topolsky@vilmate.com>
2024-09-23 12:31:35 +0300

    Fixed incorrect imports

--- src/cache_lib/__init__.py
+++ src/cache_lib/__init__.py
@@ -1,4 +1,5 @@
 __version__ = (0, 0, 1)
 
+from .async_requests import AsyncRequests  # noqa: F401
 from .models import Request  # noqa: F401
-from .requests import AsyncRequests, Requests  # noqa: F401
+from .requests import Requests  # noqa: F401
